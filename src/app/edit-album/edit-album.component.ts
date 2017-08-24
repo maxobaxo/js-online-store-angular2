23a402ffbef604013b0eefa14dececef520787b0
Max Scher and Nathan Stewart <max@>
2017-08-24 08:34:15 -0700

    add edit form functionality to admin route

--- src/app/edit-album/edit-album.component.ts
+++ src/app/edit-album/edit-album.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Album } from '../album.model';
 
 @Component({
   selector: 'app-edit-album',
   templateUrl: './edit-album.component.html',
   styleUrls: ['./edit-album.component.css']
 })
+
 export class EditAlbumComponent implements OnInit {
+  @Input() selectedAlbum;
 
   constructor() { }
 
